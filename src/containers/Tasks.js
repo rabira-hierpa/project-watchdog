213b806d79b1e80b6b41d7bc5aa2d3be0120eb33
Rabra <rabira.hierpa@gmail.com>
2018-04-27 16:33:54 +0300

    Added api call to get user info
    Made axios requtest to get current user id

--- src/containers/Tasks.js
+++ src/containers/Tasks.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Dashboard extends Component {
-  componentDidMount() {}
+  getUserid() {
+    axios
+      .request({
+        method: "get",
+        url: "/api/auth/show/current"
+      })
+      .then(response => {
+        console.log(response.data);
+        if (response.data) {
+          // window.location.href = "http://localhost:3000/tasks";
+          console.log("user logged in");
+          console.log(response.data);
+        } else {
+          console.log("user logged out");
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  componentDidMount() {
+    this.getUserid();
+  }
   render() {
     return (
       <div>
