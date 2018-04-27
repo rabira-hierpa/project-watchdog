366df18ce7e99107252d4df332035bbbdb7d1364
Rabra <rabira.hierpa@gmail.com>
2018-04-27 16:31:16 +0300

    Added NavLinks to the login page

--- src/containers/Signin.js
+++ src/containers/Signin.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import Navbar from "../components/Home/Navbar";
+import LoginForm from "../components/Home/LoginForm";
+import Footer from "../components/Home/Footer";
 class Singin extends Component {
   render() {
     return (
       <div>
         <h1>Signin page</h1>
+        <Navbar />
+        <LoginForm />
+        <Footer />
       </div>
     );
   }
