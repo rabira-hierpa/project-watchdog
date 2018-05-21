b856fc940b9bc487f95288fef51e8419321f3b21
Rabra <rabira.hierpa@gmail.com>
2018-05-21 16:39:02 +0300

    Added schedule page
    Based on task and milestone deadline dates

--- src/containers/Schedule.js
+++ src/containers/Schedule.js
@@ -9,16 +9,22 @@ class Schedule extends Component {
     super(props);
     this.props = props;
     this.state = {
-      id: ""
+      userId: "",
+      milestones: [],
+      tasks: []
     };
     this.quickadd = "Quick Add";
   }
 
-  unsafe_componentWillMount() {
-    this.getUserid();
+  UNSAFE_componentWillMount() {
+    // this.getUserid();
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.getUserid();
+    this.getMilestones();
+    this.getTasks();
+  }
 
   // Get the id of the logged in user
   getUserid() {
@@ -54,7 +60,169 @@ class Schedule extends Component {
       });
   }
 
+  // Get all Milestones
+  getMilestones() {
+    this.id = new URLSearchParams(this.props.location.search).get("id");
+    axios
+      .request({
+        method: "get",
+        url: "/api/milestones/all/" + this.id
+      })
+      .then(response => {
+        this.setState({
+          milestones: response.data.MileStone
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error: true,
+          erro_mesg:
+            "Some error occured whilet trying to fetch the data! Please try again"
+        });
+        console.log(error);
+      });
+  }
+
+  // Get all tasks of a project
+  getTasks() {
+    this.id = new URLSearchParams(this.props.location.search).get("id");
+    axios
+      .request({
+        method: "get",
+        url: "/api/tasks/all/" + this.id
+      })
+      .then(response => {
+        this.setState({ tasks: response.data.Task });
+      })
+      .catch(error => {
+        this.setState({
+          error: true,
+          erro_mesg:
+            "Some error occured whilet trying to fetch the data! Please try again"
+        });
+        console.log(error);
+      });
+  }
+
   render() {
+    let allMilestones, allTasks;
+    let oneDay = 1000 * 60 * 60 * 24;
+    if (this.state.milestones.length > 0) {
+      allMilestones = this.state.milestones.map((milestone, index) => {
+        let deadline = new Date(milestone.DeadLine);
+        let currentDate = new Date();
+        let timediff = Math.abs(deadline - currentDate);
+        let daysLeft = Math.ceil(timediff / oneDay);
+        if (milestone.Status === 1) {
+          return (
+            <tr key={index}>
+              <td> {milestone.MileStoneTitle}</td>
+              <td>
+                {new Date(milestone.DeadLine).toDateString().substr(0, 10)}
+              </td>
+              <td className="text-danger h6">
+                {deadline.getDate() >= currentDate.getDate() &&
+                deadline.getMonth() >= currentDate.getMonth() &&
+                deadline.getFullYear() >= currentDate.getFullYear()
+                  ? daysLeft + " day(s) left"
+                  : "Deadline passed " + daysLeft + " days ago"}
+              </td>
+              <td>Incomplete</td>
+            </tr>
+          );
+        } else if (milestone.Status === 2) {
+          return (
+            <tr key={index}>
+              <td> {milestone.MileStoneTitle}</td>
+              <td>
+                {new Date(milestone.DeadLine).toDateString().substr(0, 10)}
+              </td>
+              <td className="text-danger h6">
+                {deadline.getDate() >= currentDate.getDate() &&
+                deadline.getMonth() >= currentDate.getMonth() &&
+                deadline.getFullYear() >= currentDate.getFullYear()
+                  ? daysLeft + " day(s) left"
+                  : "Deadline passed " + daysLeft + " days ago"}
+              </td>
+              <td>Review</td>
+            </tr>
+          );
+        } else if (milestone.Status === 3) {
+          return (
+            <tr key={index}>
+              <td> {milestone.MileStoneTitle}</td>
+              <td>
+                {new Date(milestone.DeadLine).toDateString().substr(0, 10)}
+              </td>
+              <td className="text-success h6">{"Milestone Completed "}</td>
+              <td>Completed</td>
+            </tr>
+          );
+        } else {
+          return null;
+        }
+      });
+    }
+
+    if (this.state.tasks.length > 0) {
+      allTasks = this.state.tasks.map((task, index) => {
+        let deadline = new Date(task.DeadLine);
+        let currentDate = new Date();
+        let timediff = Math.abs(deadline - currentDate);
+        let daysLeft = Math.ceil(timediff / (1000 * 60 * 60 * 24));
+        if (task.Catagory === 1) {
+          return (
+            <tr key={index}>
+              <td> {task.TaskTitle}</td>
+              <td>{new Date(task.DeadLine).toDateString().substr(0, 10)}</td>
+              <td className="text-danger h6">
+                {deadline.getDate() > currentDate.getDate()
+                  ? daysLeft + " day(s) left"
+                  : "Deadline passed " + daysLeft + " days ago"}
+              </td>
+              <td>Todo</td>
+            </tr>
+          );
+        } else if (task.Catagory === 2) {
+          return (
+            <tr key={index}>
+              <td> {task.TaskTitle}</td>
+              <td>{new Date(task.DeadLine).toDateString().substr(0, 10)}</td>
+              <td className="text-danger  h6">
+                {deadline.getDate() > currentDate.getDate()
+                  ? daysLeft + " day(s) left"
+                  : "Deadline passed " + daysLeft + " days ago"}
+              </td>
+              <td>Inprogress</td>
+            </tr>
+          );
+        } else if (task.Catagory === 3) {
+          return (
+            <tr key={index}>
+              <td> {task.TaskTitle}</td>
+              <td>{new Date(task.DeadLine).toDateString().substr(0, 10)}</td>
+              <td className="text-danger h6">
+                {deadline.getDate() > currentDate.getDate()
+                  ? daysLeft + " day(s) left"
+                  : "Deadline passed " + daysLeft + " days ago"}
+              </td>
+              <td>Review</td>
+            </tr>
+          );
+        } else if (task.Catagory === 4) {
+          return (
+            <tr key={index}>
+              <td> {task.TaskTitle}</td>
+              <td>{new Date(task.DeadLine).toDateString().substr(0, 10)}</td>
+              <td className="text-success h6">{"Task Completed "}</td>
+              <td>Completed </td>
+            </tr>
+          );
+        } else {
+          return null;
+        }
+      });
+    }
     return (
       <div>
         <ProjectNav
@@ -72,7 +240,72 @@ class Schedule extends Component {
             <PageHeader title="Project Schedule" />
             <br />
             <div className="row justify-content-center ">
-              <div className="col-md-8 m-auto" />
+              <div className="col-md-10 m-auto">
+                <div className="table-wrapper">
+                  <table className="table table-sm table-stripped">
+                    <thead className="blue darken-3 text-white">
+                      <tr>
+                        <th scope="col">
+                          <i className="fa fa-edit pr-2" aria-hidden="true" />
+                          Milestone
+                        </th>
+                        <th scope="col">
+                          <i
+                            className="fa fa-calendar pr-2"
+                            aria-hidden="true"
+                          />
+                          Deadline Date
+                        </th>
+                        <th scope="col">
+                          <i
+                            className="fa fa-calendar-o pr-2"
+                            aria-hidden="true"
+                          />
+                          Days left
+                        </th>
+                        <th scope="col">
+                          <i className="fa fa-check pr-2" aria-hidden="true" />
+                          Status
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>{allMilestones}</tbody>
+                  </table>
+                </div>
+              </div>
+              <div className="col-md-10 ">
+                <div className="table-wrapper mt-5">
+                  <table className="table table-sm table-strippeds">
+                    <thead className="blue darken-3 text-white">
+                      <tr>
+                        <th scope="col">
+                          <i className="fa fa-edit pr-2" aria-hidden="true" />
+                          Task
+                        </th>
+                        <th scope="col">
+                          <i
+                            className="fa fa-calendar pr-2"
+                            aria-hidden="true"
+                          />
+                          Deadline Date
+                        </th>
+                        <th scope="col">
+                          <i
+                            className="fa fa-calendar-o pr-2"
+                            aria-hidden="true"
+                          />
+                          Days left
+                        </th>
+                        <th scope="col">
+                          <i className="fa fa-check pr-2" aria-hidden="true" />
+                          Status
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>{allTasks}</tbody>
+                  </table>
+                </div>
+              </div>
             </div>
           </div>
         </main>
