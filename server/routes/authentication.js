f9e26da0b3989535242268c965921e5638e2bb5d
rabra <rabira.hierpa@gmail.com>
2023-09-07 11:57:12 +0300

    fix: updates saving user logic

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,20 +1,20 @@
 // including modules to the project
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+const mongoose = require("mongoose");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
 // const keys = require('../config/keys');
-const bcrypt = require('bcryptjs');
-const LocalStrategy = require('passport-local').Strategy;
-const cookieSession = require('cookie-session');
-const ServerIp = require('../routes/ServerIP');
+const bcrypt = require("bcryptjs");
+const LocalStrategy = require("passport-local").Strategy;
+const cookieSession = require("cookie-session");
+const ServerIp = require("../routes/ServerIP");
 
 router.use(
-	cookieSession({
-		maxAge: 30 * 24 * 60 * 60 * 1000,
-		keys: ['simpleKey'],
-	})
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: ["simpleKey"],
+  })
 );
 
 router.use(passport.initialize());
@@ -22,14 +22,14 @@ router.use(passport.session());
 
 // To encode user into a cookie
 passport.serializeUser((user, done) => {
-	done(null, user.id);
+  done(null, user.id);
 });
 
 // To decode user from a cookie
 passport.deserializeUser((id, done) => {
-	userModel.findById(id).then((user) => {
-		done(null, user);
-	});
+  userModel.findById(id).then((user) => {
+    done(null, user);
+  });
 });
 
 // Using google passport strategy
@@ -89,95 +89,94 @@ passport.deserializeUser((id, done) => {
 
 // Using pasport-local strategy
 passport.use(
-	new LocalStrategy(
-		{
-			usernameField: 'Email',
-			passwordField: 'Password',
-		},
-		(Email, Password, done) => {
-			userModel
-				.findOne({
-					Email: Email,
-				})
-				.then((user) => {
-					if (!user) {
-						return done(null, false, { message: 'Incorrect Email.' });
-					}
-					// Check if password is correct
-					bcrypt.compare(Password, user.Password, (err, isMatch) => {
-						if (err) throw err;
-						if (isMatch) {
-							return done(null, user);
-						} else {
-							console.log('Incorrect Password');
-							return done(null, false, { message: 'Incorrect Password.' });
-						}
-					});
-				});
-		}
-	)
+  new LocalStrategy(
+    {
+      usernameField: "Email",
+      passwordField: "Password",
+    },
+    (Email, Password, done) => {
+      userModel
+        .findOne({
+          Email: Email,
+        })
+        .then((user) => {
+          if (!user) {
+            return done(null, false, { message: "Incorrect Email." });
+          }
+          // Check if password is correct
+          bcrypt.compare(Password, user.Password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              console.log("Incorrect Password");
+              return done(null, false, { message: "Incorrect Password." });
+            }
+          });
+        });
+    }
+  )
 );
 
 // Login route  auth/login
-router.get('/login', (req, res) => {
-	res.send({ message: 'success' });
+router.get("/login", (req, res) => {
+  res.send({ message: "success" });
 });
-router.get('/login/fail', (req, res) => {
-	res.send({ message: 'fail' });
+router.get("/login/fail", (req, res) => {
+  res.send({ message: "fail" });
 });
 
 // Login route
-router.post('/login/local', (req, res, next) => {
-	passport.authenticate('local', {
-		successRedirect: ServerIp.ipAddress + '/api/auth/login',
-		failureRedirect: ServerIp.ipAddress + '/api/auth/login/fail',
-		failureFlash: false,
-	})(req, res, next);
+router.post("/login/local", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: ServerIp.ipAddress + "/api/auth/login",
+    failureRedirect: ServerIp.ipAddress + "/api/auth/login/fail",
+    failureFlash: false,
+  })(req, res, next);
 });
 
 // Show currently loged in user
-router.get('/show/current', (req, res) => {
-	res.send(req.user);
+router.get("/show/current", (req, res) => {
+  res.send(req.user);
 });
 
 // Registor route
-router.post('/signup/local', (req, res) => {
-	userModel.findOne({ Email: req.body.Email }).then((user) => {
-		if (user) {
-			res.json({ error: 'User Already Exist' });
-		} else {
-			const newUser = new userModel({
-				Fname: req.body.Fname,
-				Lname: req.body.Lname,
-				Email: req.body.Email,
-				Password: req.body.Password,
-				Department: req.body.Department,
-				OtherDescription: req.body.OtherDescription,
-			});
+router.post("/signup/local", (req, res) => {
+  userModel.findOne({ Email: req.body.Email }).then((user) => {
+    if (user) {
+      res.json({ error: "User Already Exist" });
+    } else {
+      const newUser = new userModel({
+        Fname: req.body.Fname,
+        Lname: req.body.Lname,
+        Email: req.body.Email,
+        Password: req.body.Password,
+        Department: req.body.Department,
+        OtherDescription: req.body.OtherDescription,
+      });
 
-			bcrypt.genSalt(10, (err, salt) => {
-				bcrypt.hash(newUser.Password, salt, (err, hash) => {
-					newUser.Password = hash;
-					newUser.save(function(err, user) {
-						if (err) {
-							res.send(err);
-						} else {
-							res.json(user);
-						}
-					});
-				});
-			});
-		}
-	});
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.Password, salt, async (err, hash) => {
+          newUser.Password = hash;
+          try {
+            const user = await newUser.save();
+            res.json(user);
+          } catch (error) {
+            res.send(error);
+          }
+        });
+      });
+    }
+  });
 });
 
-router.get('/logout', (req, res) => {
-	req.logout();
-	res.send(req.user);
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.send(req.user);
 });
 
 // Loading Model
-require('../models/DBComponents');
-const userModel = mongoose.model('User');
+require("../models/DBComponents");
+const userModel = mongoose.model("User");
 
 module.exports = router;
