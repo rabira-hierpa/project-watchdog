6bc0b6158afef44abacd57bbccf3ebdf79f8aaa2
rabra <rabira.hierpa@gmail.com>
2023-12-08 11:13:56 +0300

    Add constants and helpers

---
+++ src/utils/constants/index.js
@@ -0,0 +1,12 @@
+export const BASE_URL = "http://localhost:3000";
+
+export const constants = {
+  USER: "user",
+  TOKEN: "accessToken",
+  REFRESH_TOKEN: "refreshToken",
+  EXPIRES_AT: "expiresAt",
+};
+
+export const PHONE_REGX_NUMBER_CHECK = /^[0-9]+$/;
+
+export const PHONE_REGX_CHECK = "^\\+[1-9]\\d{8,14}$";
---
+++ src/utils/helpers/index.js
@@ -0,0 +1,5 @@
+import { httpService } from "./http.service";
+import { logger } from "./logger.service";
+import { storage } from "./storage.service";
+
+export { httpService, logger, storage };
