61b55bce318fc0321330f7154e5a93e9350867f0
rabra <rabira.hierpa@gmail.com>
2023-12-08 14:23:10 +0300

    Refactor user routes to use async/await syntax

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,24 +3,26 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
-// Fetching a single user by ID from mongodb get '/users/:id'
-router.get("/:id", (req, res) => {
-  var query = userModel.findOne({ _id: req.params.id });
+// Loading Model
+require("../models/DBComponents");
+const userModel = mongoose.model("User");
 
-  query.exec(function (err, doc) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(doc);
-    }
-  });
+// Fetching a single user by ID from mongodb get '/users/:id'
+router.get("/:id", async (req, res) => {
+  try {
+    const doc = await userModel.findOne({ _id: req.params.id }).exec();
+    res.json(doc);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 // Fetching a single user name by ID from mongodb get '/users/:id'
 router.get("/name/:id", async (req, res) => {
-  var query = userModel.findOne({ _id: req.params.id }, { Fname: 1, Lname: 1 });
   try {
-    const doc = await query.exec();
+    const doc = await userModel
+      .findOne({ _id: req.params.id }, { Fname: 1, Lname: 1 })
+      .exec();
     res.json(doc);
   } catch (err) {
     res.send(err);
@@ -28,89 +30,77 @@ router.get("/name/:id", async (req, res) => {
 });
 
 // Fetching all users from mongodb get '/api/users'
-router.get("/", (req, res) => {
-  var query = userModel.find({});
-
-  query.exec(function (err, docs) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(docs);
-    }
-  });
+router.get("/", async (req, res) => {
+  try {
+    const docs = await userModel.find({}).exec();
+    res.json(docs);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 // Inserting a new user to mongodb post '/users'
-router.post("/", (req, res) => {
-  new userModel(req.body).save(function (err, doc) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(doc);
-    }
-  });
+router.post("/", async (req, res) => {
+  try {
+    const doc = await new userModel(req.body).save();
+    res.json(doc);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 // Updating a User by ID put '/users/:id'
-router.put("/:id", (req, res) => {
-  var query = userModel.update(
-    { _id: req.params.id },
-    {
-      $set: {
-        Fname: req.body.Fname,
-        Lname: req.body.Lname,
-        Email: req.body.Email,
-        Password: req.body.Password,
-        Department: req.body.Department,
-        OtherDescription: req.body.OtherDescription,
-        Type: req.body.Type,
-      },
-    }
-  );
-
-  query.exec(function (err, doc) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(doc);
-    }
-  });
+router.put("/:id", async (req, res) => {
+  try {
+    const doc = await userModel
+      .updateOne(
+        { _id: req.params.id },
+        {
+          $set: {
+            Fname: req.body.Fname,
+            Lname: req.body.Lname,
+            Email: req.body.Email,
+            Password: req.body.Password,
+            Department: req.body.Department,
+            OtherDescription: req.body.OtherDescription,
+            Type: req.body.Type,
+          },
+        }
+      )
+      .exec();
+    res.json(doc);
+  } catch (err) {
+    res.send(err);
+  }
 });
-// Block a User by ID put '/api/users/block/:id'
-router.put("/block/:id", (req, res) => {
-  var query = userModel.update(
-    { _id: req.params.id },
-    {
-      $set: {
-        Status: req.body.Status,
-      },
-    }
-  );
 
-  query.exec(function (err, doc) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(doc);
-    }
-  });
+// Block a User by ID put '/api/users/block/:id'
+router.put("/block/:id", async (req, res) => {
+  try {
+    const doc = await userModel
+      .updateOne(
+        { _id: req.params.id },
+        {
+          $set: {
+            Status: req.body.Status,
+          },
+        }
+      )
+      .exec();
+    res.json(doc);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 // Deleting a User by ID delete '/users/:id'
-router.delete("/:id", (req, res) => {
-  var query = userModel.remove({ _id: req.params.id });
-
-  query.exec(function (err, doc) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(doc);
-    }
-  });
+router.delete("/:id", async (req, res) => {
+  try {
+    const doc = await userModel.deleteOne({ _id: req.params.id }).exec();
+    res.json(doc);
+  } catch (err) {
+    res.send(err);
+  }
 });
-
-// Loading Model
-require("../models/DBComponents");
-const userModel = mongoose.model("User");
 
 module.exports = router;
