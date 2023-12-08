093b654513477bd41a2e8299c7573be32c3fafea
rabra <rabira.hierpa@gmail.com>
2023-12-08 14:22:41 +0300

    Refactor history route to use async/await

--- server/routes/history.js
+++ server/routes/history.js
@@ -3,22 +3,20 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
-// Fetching all History from a project get '/api/history/:id' id is the projectID
-router.get("/:id", (req, res) => {
-  var query = projectModel.findOne({ _id: req.params.id }, { History: 1 });
-
-  query.exec(function(err, docs) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(docs);
-    }
-  });
-});
-
-
 // Loading Model
 require("../models/DBComponents");
 const projectModel = mongoose.model("Project");
+
+// Fetching all History from a project get '/api/history/:id' id is the projectID
+router.get("/:id", async (req, res) => {
+  try {
+    const docs = await projectModel
+      .findOne({ _id: req.params.id }, { History: 1 })
+      .exec();
+    res.json(docs);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 module.exports = router;
