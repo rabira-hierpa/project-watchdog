b7e79073e66ac5e22556c9369e2a66954fe861e5
rabra <rabira.hierpa@gmail.com>
2023-12-08 14:24:07 +0300

    Refactor Schedule component to use httpService and BASE_URL constants

--- src/containers/Schedule.js
+++ src/containers/Schedule.js
@@ -4,6 +4,8 @@ import ProjectNav from "../components/Common/ProjectNav";
 import PageHeader from "../components/Common/PageHeader";
 import MainFooter from "../components/Common/MainFooter";
 import withNavigation from "../utils/wrapper/withNavigator";
+import { httpService } from "../utils/helpers";
+import { BASE_URL } from "../utils/constants";
 
 class Schedule extends Component {
   constructor(props) {
@@ -29,20 +31,19 @@ class Schedule extends Component {
 
   // Get the id of the logged in user
   getUserid() {
-    axios
-      .request({
-        method: "get",
-        url: "/api/auth/show/current",
-      })
+    httpService
+      .get(`${BASE_URL}/api/auth/show/current`)
       .then((response) => {
-        this.user = response.data;
-        this.setState({
-          userId: response.data,
-        });
+        if (response.data._id) {
+          this.setState({
+            id: response.data._id,
+          });
+          this.userName(response.data._id);
+        }
       })
       .catch((error) => {
         // User is not logged in
-        window.location.href = "http://localhost:3000/signin";
+        this.props.navigate("/signin");
       });
   }
   // Logout and reset the cookie session
