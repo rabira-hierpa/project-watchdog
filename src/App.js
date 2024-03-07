34f351482e3f7a88e032e7f711d3e80f953faf4e
rabra <rabira.hierpa@gmail.com>
2024-03-07 09:06:40 +0300

    Add AuthProvider to App component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
-
+import { AuthProvider } from "./context/auth.context";
+import "./index.css";
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Routes />
+          <AuthProvider>
+            <Routes />
+          </AuthProvider>
         </div>
       </BrowserRouter>
     );
